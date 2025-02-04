4c3aa4f25c4d0b218910d38e8540e5ccba6b476c
Anggita Wahyuningtyas <anggitagita07@gmail.com>
2025-02-04 09:21:20 +0800

    ubah ba pengoperasian jtr

--- resources/views/BeritaAcaraPengoperasian_JTR/show.blade.php
+++ resources/views/BeritaAcaraPengoperasian_JTR/show.blade.php
@@ -121,7 +121,7 @@
                             <div class="form-group row">
                                 <label for="colFormLabelSm" class="col-sm-4 col-form-label col-form-label-sm">Id JTR</label>
                                 <div class="col-sm-10">
-                                    <input type="text" name="id_jtr" class="form-control form-control-sm" id="id_jtr" placeholder="id_jtr">
+                                    <input type="text" name="id_jtr" class="form-control form-control-sm" id="id_jtr" placeholder="id_jtr" value="{{ $berita_acara_pengoperasian_jtr->id_jtr  }}" readonly>
                                 </div>
                             </div>
                             <div class="form-group row">
